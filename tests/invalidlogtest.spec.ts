735a884f35d293932fa54992b1896599ba8d197d
Fares Alahmad <fares.f.al@hotmail.com>
2026-09-12 22:30:48 +0200

    Initial Playwright tests

---
+++ tests/invalidlogtest.spec.ts
@@ -0,0 +1,18 @@
+import { expect,test ,Locator } from "@playwright/test";
+test('login test',async({page})=>{
+
+await page.goto('https://www.saucedemo.com/');
+const username: Locator = page.locator('#user-name');
+const password: Locator = page.locator('#password');
+const loginbutton: Locator = page.locator('#login-button');
+
+
+await username.fill('standard_user');
+await password.fill('sauce');
+await loginbutton.click();
+
+
+await expect(page.getByRole('button',{name: 'Dismiss error'})).toBeVisible();
+
+})
+
